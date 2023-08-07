c5af6a54de38b238f97652e8f6b4f1da567cdb46
speartip <speartip@valiantspear>
2023-08-06 19:16:23 -0700

    Added schema.sh to set up storage for engagements and tools

---
+++ schema.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Adding $HOME/Engagements"
+mkdir -p $HOME/Engagements
+echo "Checking for $HOME/Tools"
+mkdir -p $HOME/Tools
+echo 'Adding line to .bashrc to export $ENG and $TOOLS'
+echo "export ENG=$HOME/Engagements" | tee -a $HOME/.bashrc
+echo "export TOOLS=$HOME/Tools" | tee -a $HOME/.bashrc
