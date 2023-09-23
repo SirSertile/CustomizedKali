5f8e222f1189fc469ff880eac09be8b535370440
Sirsertile <rezdevhead@gmail.com>
2023-09-23 12:53:51 -0700

    added support for zsh

--- schema.sh
+++ schema.sh
@@ -5,4 +5,6 @@ echo "Checking for $HOME/Tools"
 mkdir -p $HOME/Tools
 echo 'Adding line to .bashrc to export $ENG and $TOOLS'
 echo "export ENG=$HOME/Engagements" | tee -a $HOME/.bashrc
+echo "export ENG=$HOME/Engagements" | tee -a $HOME/.zshrc
 echo "export TOOLS=$HOME/Tools" | tee -a $HOME/.bashrc
+echo "export TOOLS=$HOME/Tools" | tee -a $HOME/.zshrc
